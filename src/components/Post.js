3c41a53282a128fe221f111291e59b373e20d2ac
amnyrrd <amnyrrd@gmail.com>
2020-04-13 20:20:40 -0700

    started posts for vote and new post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,25 +1,26 @@
 import React, {useState} from 'react';
+import Vote from './Vote'
 
  const Post = (props) => {
-  let [votes, newVotes] = useState(0)
+  // let [votes, newVotes] = useState(0)
 
-  const handleUpVoteClick = () => {
-    newVotes(votes += 1)
-  }
+  // const handleUpVoteClick = () => {
+  //   newVotes(votes += 1)
+  // }
 
-  const handleDownVoteClick = () => {
-    newVotes(votes -= 1)
-  }
+  // const handleDownVoteClick = () => {
+  //   newVotes(votes -= 1)
+  // }
 
-  const loggedIn = () => {
-    // this will validate if user is logged in, and allow only one vote per user
-  }
+  // const loggedIn = () => {
+  //   // this will validate if user is logged in, and allow only one vote per user
+  // }
 
-  const positiveOrNegativeVote = (votes) => {
-    // if (votes > 0) {
-      // aiming to make this function handle showing votes next to upvote if positive, and next to downvote if negative
-    // }
-  }
+  // const positiveOrNegativeVote = (votes) => {
+  //   // if (votes > 0) {
+  //     // aiming to make this function handle showing votes next to upvote if positive, and next to downvote if negative
+  //   // }
+  // }
 
   return (
   <div>
@@ -27,9 +28,10 @@ import React, {useState} from 'react';
     <p>{props.post.content}</p>
           <p>Posted by:{props.post.user.username} ⚉ </p>
           <p>{props.post.article_link}</p>
-          <p>Current Vote Count: {votes}</p>
+          <Vote />
+          {/* <p>Current Vote Count: {votes}</p>
           <button id="upVoteButton" onClick={handleUpVoteClick}>UpVote ↑ </button><br/><br/>
-          <button id="downVoteButton" onClick={handleDownVoteClick}>DownVote ↓ </button>
+          <button id="downVoteButton" onClick={handleDownVoteClick}>DownVote ↓ </button> */}
   </div>
   );
 
